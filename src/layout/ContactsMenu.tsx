82830b60881f653b4411340dda2ebbf486fe7874
edusantanaw <eduardosantana20122002@gmail.com>
2022-12-20 10:40:28 -0300

    whatapp link

--- src/layout/ContactsMenu.tsx
+++ src/layout/ContactsMenu.tsx
@@ -8,7 +8,7 @@ const ContactsMenu = () => {
             <ul>
                 <li><a href='https://www.instagram.com/edusantanaw/'><BsInstagram color="#a90abb" /></a></li>
                 <li><a href="https://www.linkedin.com/in/eduardo-santana-7a4926245/"><BsLinkedin color="#0a29c4" /></a></li>
-                <li><BsWhatsapp color="#0d861b" /></li>
+                <li><a href="https://wa.me/15981026465"><BsWhatsapp color="#0d861b" /></a></li>
                 <li><a href="https://github.com/edusantanaw"><BsGithub color="#61105d" /></a></li>
             </ul>
         </Container>
